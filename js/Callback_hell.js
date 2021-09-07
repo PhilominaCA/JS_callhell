689436568ce3b70d2576e64040ed41a89f6fdb23
unknown <philomina.jeni@gmail.com>
2021-09-07 21:27:28 +0530

    updated style

--- js/Callback_hell.js
+++ js/Callback_hell.js
@@ -1,27 +1,38 @@
 var countdownfunc = function() {
     var count = 10;
     setTimeout(function() {
-        console.log(count);
+        document.getElementById("timer").innerHTML = count;
+        // console.log(count);
         setTimeout(function() {
-            console.log(count - 1);
+            document.getElementById("timer").innerHTML = count - 1;
+            //console.log(count - 1);
             setTimeout(function() {
-                console.log(count - 2);
+                document.getElementById("timer").innerHTML = count - 2;
+                //console.log(count - 2);
                 setTimeout(function() {
-                    console.log(count - 3);
+                    document.getElementById("timer").innerHTML = count - 3;
+                    // console.log(count - 3);
                     setTimeout(function() {
-                        console.log(count - 4);
+                        document.getElementById("timer").innerHTML = count - 4;
+                        //console.log(count - 4);
                         setTimeout(function() {
-                            console.log(count - 5);
+                            document.getElementById("timer").innerHTML = count - 5;
+                            //  console.log(count - 5);
                             setTimeout(function() {
-                                console.log(count - 6);
+                                document.getElementById("timer").innerHTML = count - 6;
+                                //    console.log(count - 6);
                                 setTimeout(function() {
-                                    console.log(count - 7);
+                                    document.getElementById("timer").innerHTML = count - 7;
+                                    //      console.log(count - 7);
                                     setTimeout(function() {
-                                        console.log(count - 8);
+                                        document.getElementById("timer").innerHTML = count - 8;
+                                        //        console.log(count - 8);
                                         setTimeout(function() {
-                                            console.log(count - 9);
+                                            document.getElementById("timer").innerHTML = count - 9;
+                                            //          console.log(count - 9);
                                             setTimeout(function() {
-                                                console.log("Happy Independence Day");
+                                                document.getElementById("timer").innerHTML = "Happy Independence Day";
+                                                //            console.log("Happy Independence Day");
                                             }, 1000);
                                         }, 1000);
                                     }, 1000);
